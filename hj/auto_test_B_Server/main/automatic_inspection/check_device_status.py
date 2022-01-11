de88ac5ffdf7b0c1ab2b41c128001e1977def0ed
洪建 <hong19920627@163.com>
2022-01-11 13:35:52 +0800

    1.设备状态检测类

--- hj/auto_test_B_Server/main/automatic_inspection/check_device_status.py
+++ hj/auto_test_B_Server/main/automatic_inspection/check_device_status.py
@@ -1,5 +1,29 @@
 '''
 @Author：洪建
 @Date：2022/1/11 10:54
-该包用于检查设备状态，基于
+该包用于检查设备状态，基于B_QueryFaciDevStat能力来获取设备状态
 '''
+from component.manage_xml import ManageXml
+from main.base_component.request_interface import RequestInterface
+
+
+class CheckDeviceStatus(object):
+
+    def __init__(self):
+        pass
+
+    # 获取设备的状态，报文中的state参数的值
+    def check_device_status(self, equid):
+        # 获取B_QueryFaciDevStat的响应报文
+        xml_string = RequestInterface().get_B_QueryFaciDevStat(equid)
+        # 实例化xml操作类
+        mx = ManageXml(xml_string=xml_string)
+        # 获取state标签的值
+        # 返回实际状态
+        return mx.root[1].find('.//srrc:state', mx.ns).text
+
+
+
+if __name__ == '__main__':
+    cds = CheckDeviceStatus()
+    print(cds.check_device_status('a6590676-f5a7-4220-b105-a6ea4edbd77b'))
--- hj/auto_test_B_Server/main/base_component/request_interface.py
+++ hj/auto_test_B_Server/main/base_component/request_interface.py
@@ -4,8 +4,12 @@
 通用接口调用方法
 '''
 import requests as requests
-from component.manage_xml import ManageXml
 
+from component.loading_config import database_config
+from component.manage_xml import ManageXml
+from component.sqlite_method import ManageSqlite
+
+DATABASE_NAME = database_config()
 
 class RequestInterface(object):
     '''
@@ -32,7 +36,29 @@ class RequestInterface(object):
         # 返回B_QueryDeviceInfo响应报文
         return self.re.post(url=url, data=xml_message, headers=self.headers).text
 
+    # B_QueryFaciDevStat的通用方法
+    def get_B_QueryFaciDevStat(self, equid):
+        # 实例化xml操作类
+        xm = ManageXml(xml_file_name='B_QueryFaciDevStat.xml')
+        # 根据equid查询设备的基本信息
+        ms = ManageSqlite(DATABASE_NAME)
+        ip, port, mfid, equname = ms.get_fetchone(
+            "SELECT ip,port,mfid,equname FROM device WHERE equid = '%s'" % equid)
+        ms.close_cur_conn()
+        # 替换请求报文中的mfid,equid
+        xm.root[1].find('.//srrc:mfid', xm.ns).text = mfid
+        xm.root[1].find('.//srrc:equid', xm.ns).text = equid
+        # 保存修改
+        xm.write_xml()
+        # 获取xml报文的字符串
+        xml_message = xm.get_xml_string(xm.temp_path).encode(encoding='utf-8')
+        # 组装url
+        url = 'http://%s:%s/%s/%s/%s' % (ip, port, mfid, equname, 'B_QueryFaciDevStat')
+        self.headers['SOAPAction'] = 'B_QueryFaciDevStat'
+
+        #返回B_QueryFaciDevStat的响应报文
+        return self.re.post(url=url, data=xml_message, headers=self.headers).text
 
 if __name__ == '__main__':
     ri = RequestInterface()
-    print(ri.get_B_QueryDeviceInfo('a6590676-f5a7-4220-b105-a6ea4edbd77b'))
+    print(ri.get_B_QueryFaciDevStat('a6590676-f5a7-4220-b105-a6ea4edbd77b'))
