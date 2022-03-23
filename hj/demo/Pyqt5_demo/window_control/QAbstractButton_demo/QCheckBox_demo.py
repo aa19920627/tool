30aa04d60d9f60aec45245100bb2c8586e68f175
hong <719199445@qq.com>
2022-03-23 14:54:50 +0800

    pyqt5的demo

--- hj/demo/Pyqt5_demo/window_control/QAbstractButton_demo/QCheckBox_demo.py
+++ hj/demo/Pyqt5_demo/window_control/QAbstractButton_demo/QCheckBox_demo.py
@@ -2,8 +2,10 @@
 @Author：洪建
 @Date：2022/3/22 16:28
 '''
-from PyQt5 import Qt
-from PyQt5.QtWidgets import QWidget, QGroupBox, QHBoxLayout, QCheckBox, QVBoxLayout
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QGroupBox, QHBoxLayout, QCheckBox, QVBoxLayout, QApplication
 
 '''
 QCheckBox示例
@@ -34,6 +36,8 @@ class QCheckBox_Demo1(QWidget):
         self.checkbox3.stateChanged.connect(lambda: self.btnstate(self.checkbox3))
         layout.addWidget(self.checkbox3)
 
+        gbox.setLayout(layout)
+
         mainlayout = QVBoxLayout(self)
         mainlayout.addWidget(gbox)
 
@@ -46,4 +50,10 @@ class QCheckBox_Demo1(QWidget):
             self.checkbox2.checkState()) + "\n"
         chk3_status = self.checkbox3.text() + ", ischecked=" + str(self.checkbox3.isChecked()) + "checkstate=" + str(
             self.checkbox3.checkState()) + "\n"
-        print(chk1_status)
+        print(chk1_status,chk2_status ,chk3_status)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = QCheckBox_Demo1()
+    win.show()
+    sys.exit(app.exec_())
