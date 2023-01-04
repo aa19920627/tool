31b5529e52aa2bd0cb573bea08980f92d75b4dbc
洪建 <hong19920627@163.com>
2023-01-04 14:47:58 +0800

    word批量转pdf工具

---
+++ hj/small_tool/main/document_conversion.py
@@ -0,0 +1,21 @@
+'''
+@Author: 洪建
+@Date 2023/1/4 14:17
+'''
+
+'''
+文档转换工具
+'''
+
+import office
+
+def word_conversion_pdf():
+    '''
+    wor转pdf
+    '''
+    path = '../data'
+    office.word.docx2pdf(path=path)
+
+if __name__ == '__main__':
+    
+    # word_conversion_pdf()
