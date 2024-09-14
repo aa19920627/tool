9de393bf7b806c7ca699e42e46bc3b259a259a21
hongjian <719199445@qq.com>
2024-09-14 09:47:12 +0800

    dev分支上删掉工具包

--- equipment_restart/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-@File    : __init__.py.py
-@Date    : 2021/1/25 17:04
-@Author  : 洪建
-"""
--- equipment_restart/ui/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-@File    : __init__.py.py
-@Date    : 2021/1/25 9:55
-@Author  : 洪建
-"""
